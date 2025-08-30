4076814955e4445f11579d236fddb3aae44b8343
GuiRBeira <guilhermericardobeira@gmail.com>
2025-08-30 02:14:01 -0300

    Bot encerrado no Windows-x64-AMD-6285d4

--- src/coliseu/sorteios.js
+++ src/coliseu/sorteios.js
@@ -34,7 +34,7 @@ function adicionarArena(nome, descricao) {
 function listarArenasDisponiveis() {
   const arenas = carregarArenas()
       // Retorna uma string legível
-    return arenas.map(a => `• ${a.nome} ${a.disponivel ? "✅" : "❌"}\n`).join("\n\n");
+    return arenas.map(a => `• ${a.nome} ${a.disponivel ? "✅" : "❌"}\n`).join("\n");
 }
 
 // Alterar disponibilidade de uma arena
