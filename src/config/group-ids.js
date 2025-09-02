51d066f8bb771bbb213a220136d3749d8b337398
GuiRBeira <guilhermericardobeira@gmail.com>
2025-09-01 21:04:56 -0300

    arenas do coliseu add

--- src/config/group-ids.js
+++ src/config/group-ids.js
@@ -1,33 +1,37 @@
 module.exports = {
-    groupIds: [
-      '120363296203601720@g.us',
-      '556384043631-1485535224@g.us',
-      '120363418752793626@g.us' //teste
-    ],
-    vila: '120363274289996614@g.us',
-    coliseu: [
-      '120363402674312295@g.us' //Organização do Coliseu
-    ],
-    passe: [
-      '120363419587670596@g.us',
-      '120363418978503809@g.us',
-      '556384043631-1485535224@g.us', // academia
-      '120363301387431896@g.us', //mercado negro
-      '554888043300@s.whatsapp.net', // bruna mercado
-      '120363294193453338@g.us', // arena ninja
-      '120363351084211015@g.us' //grupo do prodigio
-    ],
-    arenaspublicas: [
-      '120363294193453338@g.us', // arena ninja
-      '120363295275504328@g.us', // arena superior
-      '557591801784-1489240947@g.us', // arena kumo
-      '556291280409-1566962636@g.us', // arena iwa
-    ],
-    clans: [
-      ''
-    ],
-    cupula: [
-      '120363317644374250@g.us' //cupula de ame
-    ]
-  }
-  
+  groupIds: [
+    '120363296203601720@g.us',
+    '556384043631-1485535224@g.us',
+    '120363418752793626@g.us' //teste
+  ],
+  vila: '120363274289996614@g.us',
+  coliseu: [
+    '120363402674312295@g.us', //Organização do Coliseu
+    '120363401059338890@g.us',
+    '120363402938880393@g.us',
+    '120363421102553163@g.us',
+    '120363403381879874@g.us',
+    '120363420521855270@g.us',
+  ],
+  passe: [
+    '120363419587670596@g.us',
+    '120363418978503809@g.us',
+    '556384043631-1485535224@g.us', // academia
+    '120363301387431896@g.us', //mercado negro
+    '554888043300@s.whatsapp.net', // bruna mercado
+    '120363294193453338@g.us', // arena ninja
+    '120363351084211015@g.us' //grupo do prodigio
+  ],
+  arenaspublicas: [
+    '120363294193453338@g.us', // arena ninja
+    '120363295275504328@g.us', // arena superior
+    '557591801784-1489240947@g.us', // arena kumo
+    '556291280409-1566962636@g.us', // arena iwa
+  ],
+  clans: [
+    ''
+  ],
+  cupula: [
+    '120363317644374250@g.us' //cupula de ame
+  ]
+}
